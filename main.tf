208c6390a71ecaf707a2dbf3ae1f5559f6eedaa8
fuku <mmafighter_shun@icloud.com>
2020-01-03 21:29:41 +0900

    次のフェーズに向け一旦削除
    f

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
 provider "aws" {
     region = "ap-northeast-1"
 }
-
-module "web_server" {
-  source        = "./http_server"
-  instance_type = "t3.micro"
-}
-
-output "public_dns" {
-  value = module.web_server.public_dns
-}
