04a5487414cd83c70bcd11f3545ee77dd2fff89b
fuku <mmafighter_shun@icloud.com>
2020-01-04 17:02:45 +0900

    ECS定義追加（batch処理タスク）

--- ecs.tf
+++ ecs.tf
@@ -14,6 +14,16 @@ resource "aws_ecs_task_definition" "example" {
   execution_role_arn       = module.ecs_task_execution_role.iam_role_arn
 }
 
+resource "aws_ecs_task_definition" "example_batch" {
+  family                   = "example-batch"
+  cpu                      = "256"
+  memory                   = "512"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  container_definitions    = file("./batch_container_definitions.json")
+  execution_role_arn       = module.ecs_task_execution_role.iam_role_arn
+}
+
 resource "aws_ecs_service" "example" {
   name                              = "example"
   cluster                           = aws_ecs_cluster.example.arn
@@ -59,8 +69,17 @@ resource "aws_cloudwatch_log_group" "for_ecs" {
   retention_in_days = 180
 }
 
+resource "aws_cloudwatch_log_group" "for_ecs_scheduled_tasks" {
+  name              = "/ecs-scheduled-tasks/example"
+  retention_in_days = 180
+}
+
 data "aws_iam_policy" "ecs_task_execution_role_policy" {
   arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+data "aws_iam_policy" "ecs_events_role_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceEventsRole"
 }
 
 data "aws_iam_policy_document" "ecs_task_execution" {
@@ -73,6 +92,31 @@ data "aws_iam_policy_document" "ecs_task_execution" {
   }
 }
 
+resource "aws_cloudwatch_event_rule" "example_batch" {
+  name                = "example-batch"
+  description         = "とても重要なバッチ処理です"
+  schedule_expression = "cron(*/2 * * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "example_batch" {
+  target_id = "example-batch"
+  rule      = aws_cloudwatch_event_rule.example_batch.name
+  role_arn  = module.ecs_events_role.iam_role_arn
+  arn       = aws_ecs_cluster.example.arn
+
+  ecs_target {
+    launch_type         = "FARGATE"
+    task_count          = 1
+    platform_version    = "1.3.0"
+    task_definition_arn = aws_ecs_task_definition.example_batch.arn
+
+    network_configuration {
+      assign_public_ip = "false"
+      subnets          = [aws_subnet.private_0.id]
+    }
+  }
+}
+
 module "ecs_task_execution_role" {
   source     = "./iam_role"
   name       = "ecs-task-execution"
@@ -80,3 +124,9 @@ module "ecs_task_execution_role" {
   policy     = data.aws_iam_policy_document.ecs_task_execution.json
 }
 
+module "ecs_events_role" {
+  source     = "./iam_role"
+  name       = "ecs-events"
+  identifier = "events.amazonaws.com"
+  policy     = data.aws_iam_policy.ecs_events_role_policy.policy
+}
